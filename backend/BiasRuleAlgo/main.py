427f5132e22a86744fe776e45a4614ac22a4a6b2
MikeyMictan1 <michaeltansikorski@gmail.com>
2026-06-05 10:30:20 +0100

    skeleton explanation

---
+++ backend/BiasRuleAlgo/main.py
@@ -0,0 +1,21 @@
+# DESIGNING THE RULE-BASED ALGORITHM:
+# Rule Based Algo -> Very Basic Bias Detection | It also gets passed into the API after for better context.
+
+# 1) Words with weights in a dictionary, 
+# words from the AI response get flagged + depending on context questions answered.
+flagged_words_maths = {
+    "very bad word" : 100,
+    "ok" : 1
+}
+
+flagged_words_humanities = {
+    "very bad word" : 1,
+    "ok" : 100
+}
+
+# 2) Idea two
+
+
+
+
+
